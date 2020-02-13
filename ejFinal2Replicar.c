411fb66c55b38aee6765988eab119e71988b331b
Diego <diego.balestieri1998@gmail.com>
2020-02-13 20:07:56 -0300

    agrego otra manera de hacer ese ej

--- ejFinal2Replicar.c
+++ ejFinal2Replicar.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+/*
 void replicar(const char* palabra, int posInicio, int posFinal, int cantRepeticiones) {
 	size_t largoOriginal = strlen(palabra);
 	printf("%s - %i - %i - %i\n", palabra, posInicio, posFinal, cantRepeticiones);
@@ -29,12 +29,42 @@ void replicar(const char* palabra, int posInicio, int posFinal, int cantRepetici
 	palabraFinal[largoFinal-1] = '\0';
 	printf("La cadena final es: %s\n", palabraFinal);
 }
+*/
+
+char* replicar(const char* palabra, int i1, int i2, int cantRepeticiones) {
+	if(palabra == NULL || i1 >= i2 || cantRepeticiones < 0){return NULL;}
+	size_t lenOrg = strlen(palabra);
+	if(lenOrg <= 0 || i2 >= lenOrg){return NULL;}
+	size_t tam = lenOrg+(i2-i1+1)*(cantRepeticiones-1);
+	char* nuevo = malloc(sizeof(char)*tam);
+	for(int i = 0; i < i1; ++i){
+		nuevo[i] = palabra[i];
+	}
+	int pos = i1;
+	for(int i = 0; i < cantRepeticiones; ++i){
+		for(int j = i1; j <= i2; ++j){
+			nuevo[pos] = palabra[j];
+			++pos;
+		}
+	}
+	for(int i = i2+1; i < lenOrg; ++i){
+		nuevo[pos] = palabra[i];
+		++pos;
+	}
+	return nuevo;
+}
 
 int main(int argc, char const *argv[]) {
 	if(argc != 5){
 		fprintf(stderr, "Error en los parametros ingresados\n");
 		return -1;
 	}
-	replicar(argv[1], atoi(argv[2]), atoi(argv[3]), atoi(argv[4]));
+	char* aux = replicar(argv[1], atoi(argv[2]), atoi(argv[3]), atoi(argv[4]));
+	if(aux == NULL){
+		printf("Es null\n");
+	} else {
+		printf("%s\n", aux);
+	}
+	free(aux);
 	return 0;
 }
